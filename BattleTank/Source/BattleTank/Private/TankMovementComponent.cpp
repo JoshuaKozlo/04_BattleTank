36d369d0905a84aa7201dacfbdc70961ded7f23a
DESKTOP-GH3Q1KG\joshu <joshua.kozlo@gmail.com>
2018-01-19 15:07:22 -0500

    Can now move tank forward and backward with left analog stick

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,12 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	// auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("Inted move forward throw: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
 
 
