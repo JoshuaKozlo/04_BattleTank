c31942a8274118b69db2713f2c6e7bce25044afb
DESKTOP-GH3Q1KG\joshu <joshua.kozlo@gmail.com>
2018-01-24 19:34:25 -0500

    Tank no longer slides sideways while turning.

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -2,24 +2,29 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/StaticMeshComponent.h"
 #include "TankTrack.generated.h"
 
 /**
- * 
- */
+* TankTrack is used to set maximum driving force, and to apply forces to the tank.
+*/
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
+
 public:
 	// Sets a throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = "Input")
-	void SetThrottle(float Throttle);
-	
-	// MAx force per track, in Newtons
+		void SetThrottle(float Throttle);
+
+	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 40000000; // Asume ton tank, and 1g accelleration
+		float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g accelleration
+
+private:
+	UTankTrack();
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
 };
